82c81df533cdcd2df50628919646f535fdd6b0db
Paul Henrich <paul.henrich@blackbaud.com>
2023-09-21 15:20:08 -0400

    Add game

---
+++ game/lib/game.ex
@@ -0,0 +1,30 @@
+defmodule Game do
+  def new(sentence, steps) do
+    length = String.length(sentence)
+    chunk_size = (length / steps) |> ceil
+    steps = Enum.shuffle(1..length) |> Enum.chunk_every(chunk_size)
+    %{original: sentence, sentence: sentence, steps: steps}
+  end
+
+  def erase(game) do
+    sentence = String.graphemes(game.sentence) |> Enum.with_index(1)
+    [step | tail] = game.steps
+    # sentence {"a", 0}, {"b", 1}, {"c", 2}
+    new_sentence =
+      Enum.map(sentence, fn {char, sentence_i} ->
+        cond do
+          char in [".", ",", "'", " "] -> char
+          sentence_i in step -> "_"
+          true -> char
+        end
+      end)
+      |> Enum.join
+
+    %{game | steps: tail, sentence: new_sentence}
+  end
+
+  def show(game) do
+    game.sentence
+  end
+
+end
